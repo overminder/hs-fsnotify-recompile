a25d92c22291f6d1bdd39fc9f79ff7e27e260887
overminder <p90eri@gmail.com>
2014-10-09 17:55:10 +0800

    Using Yaml to do configuration

--- Main.hs
+++ Main.hs
@@ -1,13 +1,15 @@
-{-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE RecordWildCards, OverloadedStrings, CPP #-}
 
 import Control.Concurrent
 import Control.Applicative
 import Control.Monad
+import Data.String
 import System.Process (runCommand)
 import Filesystem.Path.CurrentOS (filename, encodeString)
 import System.FSNotify
 import System.FilePath.Glob
 import System.Environment
+import qualified Data.Yaml.Config as Y
 
 pathOf evt = case evt of
   Added path _ -> path
@@ -17,6 +19,31 @@ pathOf evt = case evt of
 isModified (Modified {}) = True
 isModified _ = False
 
+isAdded (Added {}) = True
+isAdded _ = False
+
+#if linux_HOST_OS
+acceptEvent = isModified
+#elif mingw32_HOST_OS
+acceptEvent = const True
+#elif darwin_HOST_OS
+acceptEvent = isAdded
+#else
+# error Unknown OS
+#endif
+
+data Config = Config
+  { recompCmd :: String
+  , dirToWatch :: String
+  , rawGlobs :: [String]
+  }
+
+loadConfig :: Y.Config -> Either Y.KeyError Config
+loadConfig config = Config
+  <$> Y.lookup "Recompilation command" config
+  <*> Y.lookup "Directory to watch" config
+  <*> Y.lookup "File globs to watch" config
+
 main = do
   args <- getArgs
   let
@@ -24,30 +51,35 @@ main = do
       [conf] -> conf
       _ -> ".fsnotify-recompile.conf"
   putStrLn $ "[Main] Conf: " ++ confPath
-  (recompCmd:rawGlobs) <- lines <$> readFile confPath
+  eiConfig <- loadConfig <$> Y.load confPath
+  case eiConfig of
+    Left e -> putStrLn $ "[Error] " ++ show e
+    Right config -> runWithConfig config
+
+runWithConfig Config {..} = do
   putStrLn $ "[Main] Recompilation command: " ++ recompCmd
   putStrLn $ "[Main] Globs: " ++ show rawGlobs
   let
     patterns = map compile rawGlobs
     onEvt evt = do
-      if isModified evt
-        then do
-          putStrLn $ "[Event] " ++ show evt
-          let
-            path = pathOf evt
-            relName = encodeString (filename path)
-          oks <- forM patterns $ \ pat -> do
-            let ok = match pat relName
-            when ok $
-              putStrLn $ "[Matched] " ++ relName ++ " against " ++ show pat
-            return ok
-          when (any id oks) $ do
-            putStrLn $ "[Recomp] Running " ++ recompCmd
-            runCmd
-        else return ()
+      putStrLn $ "[Event] " ++ show evt
+      let
+        path = pathOf evt
+        relPath = encodeString (filename path)
+      oks <- forM patterns $ \ pat -> do
+        let ok = match pat relPath
+        when ok $
+          putStrLn $ "[Matched] " ++ relPath ++ " against " ++ show pat
+        return ok
+      when (any id oks) $ do
+        putStrLn $ "[Recomp] Running " ++ recompCmd
+        runCmd
     runCmd = void $ runCommand recompCmd
 
   withManager $ \ mgr -> do
-    watchTree mgr "." (const True) onEvt
-    forever $ threadDelay maxBound
+    watchTree mgr (fromString dirToWatch) acceptEvent onEvt
+    forever $ threadDelay (1024 * 1000000)
+    -- ^ See #7235 (https://ghc.haskell.org/trac/ghc/ticket/7325#comment:4)
+    -- OSX is buggy on `threadDelay maxBound`
+
 
